191ec2be4e8469091402ebcbb393f093069b8ab3
parhampch <parhampch@gmail.com>
2021-10-19 14:58:11 +0330

    complete room module

--- hw2/room.v
+++ hw2/room.v
@@ -3,8 +3,44 @@ module room (
     input out,
     input ent,
     input T,
+    input clk,
     output open,
     output close);
+
+    wire en, up, clr;
+    wire [3:0] number,
+
+    counter people (en , up, clr, clk, number);
+
+    always @(posedge clk) 
+    begin
+        if (T && in && ent && number < 15 ) 
+        begin
+            if (out)
+                en <= 0;
+            else
+            begin
+                en <= 1;
+                up <= 1;
+            end
+            close <= 0;
+        end
+        else if (out) 
+        begin
+            en <= 1;
+            up <= 0;
+            if (number == 0)
+                close <= 1;
+            else
+                close <= 0;
+        end    
+        else
+        begin
+           en <= 0;
+           close <= 0; 
+        end
+    end
+
     
 endmodule
 module counter (
